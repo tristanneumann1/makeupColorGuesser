b4f57f71dbdd3513e7678b1d55c680e9be047af8
Tristan Neumann <tristan.neumann@hotmail.com>
2019-01-25 20:04:07 -0800

    Styles whole page, No multi click code break;

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 const $colorName = $('#color-name');
 const $color1 = $('#color1');
+const $color1Text = $('#text-color1');
 const $color2 = $('#color2');
+const $color2Text = $('#text-color2');
 let wins = 0;
 let losses = 0;
 let correctColor;
+let inClick = false;
 
 function start() {
   wins = 0;
@@ -13,8 +16,8 @@ function start() {
 }
 
 function newColor() {
-  $color1.text('');
-  $color2.text('');
+  $color1Text.text('');
+  $color2Text.text('');
   let i = Math.floor(Math.random() * colors.length);
   let j = Math.floor(Math.random() * colors.length);
   while(j === i) {
@@ -24,14 +27,14 @@ function newColor() {
   $colorName.text(correctColor); 
   if(Math.random() > 0.5) {
     $color1.css('backgroundColor', colors[i].hex_value);
-    $color1.attr('name', colors[i].colour_name);
+    $color1Text.attr('name', colors[i].colour_name);
     $color2.css('backgroundColor', colors[j].hex_value);
-    $color2.attr('name', colors[j].colour_name);
+    $color2Text.attr('name', colors[j].colour_name);
   } else {
     $color2.css('backgroundColor', colors[i].hex_value);
-    $color2.attr('name', colors[i].colour_name);
+    $color2Text.attr('name', colors[i].colour_name);
     $color1.css('backgroundColor', colors[j].hex_value);
-    $color1.attr('name', colors[j].colour_name);
+    $color1Text.attr('name', colors[j].colour_name);
   }
 }
 
@@ -41,14 +44,18 @@ function updateScore() {
 }
 
 function displaySollution() {
-  $color1.text($color1.attr('name'));
-  $color2.text($color2.attr('name'));
+  $color1Text.text($color1Text.attr('name'));
+  $color2Text.text($color2Text.attr('name'));
   return new Promise((resolve) => {
     setTimeout(resolve, 2000);
   })
 }
 
 function selectColor(e) {
+  if(inClick) {
+    return;
+  }
+  inClick = true;
   if ($(this).attr('name') === correctColor) {
     wins++;
   } else {
@@ -57,6 +64,7 @@ function selectColor(e) {
   displaySollution().then( () => {
       updateScore();
       newColor();
+      inClick = false;
     }
   )
 }
